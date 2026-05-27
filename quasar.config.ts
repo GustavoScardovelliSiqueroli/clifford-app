a11469ee68bc370cc87d7c6793ffef932cdf9180
gustavo <gustavosecardovelli@gmail.com>
2026-05-27 09:01:30 -0300

    chore: setup android mode with sqlite

--- quasar.config.ts
+++ quasar.config.ts
@@ -178,6 +178,7 @@ export default defineConfig((/* ctx */) => {
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/developing-capacitor-apps/configuring-capacitor
     capacitor: {
       hideSplashscreen: true,
+      capacitorCliPreparationParams: ['sync', 'android'],
     },
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/developing-electron-apps/configuring-electron
