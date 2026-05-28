d1c5b3fe0e1dde9f04d004630e6c0ef50f5351c1
gustavo <gustavosecardovelli@gmail.com>
2026-05-28 14:57:12 -0300

    feat: (temporary) button to reset db for development

--- src/pages/SettingsPage.vue
+++ src/pages/SettingsPage.vue
@@ -1,1 +1,19 @@
-<template><q-page></q-page></template>
+<template>
+  <q-page class="fit column bg-primary align-center justify-center">
+    <div class="row align-center justify-center">
+      <q-btn color="negative" label="RESET DB" @click="resetDb"></q-btn>
+    </div>
+  </q-page>
+</template>
+<script setup lang="ts">
+import { getDB, saveDB } from 'src/database/connection';
+import { runMigrations } from 'src/database/migrations';
+
+async function resetDb() {
+  const db = await getDB();
+  await db.execute('DROP TABLE clientes');
+  await saveDB();
+  await runMigrations();
+  await saveDB();
+}
+</script>
