3bd0850c259b9d624c45047963d53cdc1a509a66
gustavo <gustavosecardovelli@gmail.com>
2026-07-17 16:46:19 -0300

    feat: notification toast

--- quasar.config.ts
+++ quasar.config.ts
@@ -100,7 +100,14 @@ export default defineConfig((/* ctx */) => {
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#framework
     framework: {
-      config: {},
+      config: {
+        notify: {
+          position: 'top',
+          timeout: 3000,
+          progress: true,
+          actions: [{ icon: 'close', color: 'white' }],
+        },
+      },
 
       // iconSet: 'material-icons', // Quasar icon set
       // lang: 'en-US', // Quasar language pack
@@ -113,7 +120,7 @@ export default defineConfig((/* ctx */) => {
       // directives: [],
 
       // Quasar plugins
-      plugins: [],
+      plugins: ['Notify'],
     },
 
     // animations: 'all', // --- includes all animations
